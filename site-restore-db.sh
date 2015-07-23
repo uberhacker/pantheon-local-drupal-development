8a539479820320bf54ee5f437e6869f19f5c0464
Ed Reel <ereel@eaglecrk.com>
2015-07-23 02:11:00 +0000

    Added multisite detection to site-restore-db.sh

--- site-restore-db.sh
+++ site-restore-db.sh
@@ -10,7 +10,48 @@ if test $1; then
     exit
   fi
   cd /var/www/$SITENAME
-  drush sqlc < dev-$SITENAME.sql
+
+  # Set multisite
+  MULTISITES=""
+  DEFAULTSITE="default"
+  cd /var/www/$SITENAME/sites
+  SITES=$(echo $(ls -d */) | sed 's,/,,g')
+  for SITE in $SITES; do
+    if [[ "$SITE" != "all" && -f "/var/www/$SITENAME/sites/$SITE/settings.php" ]]; then
+      if [ -z "$MULTISITES" ]; then
+        MULTISITES="$SITE"
+      else
+        MULTISITES="$MULTISITES $SITE"
+      fi
+      DEFAULTSITE="$SITE"
+    fi
+  done
+  if [ "$DEFAULTSITE" == "$MULTISITES" ]; then
+    MULTISITE="$DEFAULTSITE"
+  fi
+  if [[ "$MULTISITE" == "default" && "$MULTISITES" != "default" ]]; then
+    echo ""
+    echo "The following multisites are available:"
+    echo $MULTISITES
+    echo ""
+    echo -n "Enter the multisite ($DEFAULTSITE): "; read MULTISITE
+    if [ -z "$MULTISITE" ]; then
+      MULTISITE="$DEFAULTSITE"
+    fi
+  fi
+  if [ "$MULTISITE" != "$DEFAULTSITE" ]; then
+    VALID=no
+    for MULTI in $MULTISITES; do
+      if [ "$MULTI" == "$MULTISITE" ]; then
+        VALID=yes
+      fi
+    done
+    if [ "$VALID" == "no" ]; then
+      echo "$MULTISITE is not a valid multisite."
+      exit
+    fi
+  fi
+  drush -l $MULTISITE sqlc < dev-$SITENAME.sql
 else
   echo ""
   echo "Purpose: Restores the database back to the state of the initial site install"
