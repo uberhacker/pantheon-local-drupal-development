192797dc5e13f4e0805d598da2b7a96da3308830
Ed Reel <ereel@eaglecrk.com>
2015-07-23 02:10:37 +0000

    Added multisite detection to site-fix.sh

--- site-fix.sh
+++ site-fix.sh
@@ -12,7 +12,48 @@ if test $1; then
   cd /var/www/$SITENAME
   sudo chown -R vagrant:www-data sites/default/files
   sudo chmod -R g+w sites/default/files
-  drush rr
+
+  # Set multisite
+  MULTISITES=""
+  DEFAULTSITE="default"
+  cd /var/www/$SITENAME/sites
+  SITES=$(echo $(ls -d */) | sed 's,/,,g')
+  for SITE in $SITES; do
+    if [[ "$SITE" != "all" && -f "/var/www/$SITENAME/sites/$SITE/settings.php" ]]; then
+      if [ -z "$MULTISITES" ]; then
+        MULTISITES="$SITE"
+      else
+        MULTISITES="$MULTISITES $SITE"
+      fi
+      DEFAULTSITE="$SITE"
+    fi
+  done
+  if [ "$DEFAULTSITE" == "$MULTISITES" ]; then
+    MULTISITE="$DEFAULTSITE"
+  fi
+  if [[ "$MULTISITE" == "default" && "$MULTISITES" != "default" ]]; then
+    echo ""
+    echo "The following multisites are available:"
+    echo $MULTISITES
+    echo ""
+    echo -n "Enter the multisite ($DEFAULTSITE): "; read MULTISITE
+    if [ -z "$MULTISITE" ]; then
+      MULTISITE="$DEFAULTSITE"
+    fi
+  fi
+  if [ "$MULTISITE" != "$DEFAULTSITE" ]; then
+    VALID=no
+    for MULTI in $MULTISITES; do
+      if [ "$MULTI" == "$MULTISITE" ]; then
+        VALID=yes
+      fi
+    done
+    if [ "$VALID" == "no" ]; then
+      echo "$MULTISITE is not a valid multisite."
+      exit
+    fi
+  fi
+  drush -l $MULTISITE rr
 else
   echo ""
   echo "Purpose: Repairs the site database and file permissions"
