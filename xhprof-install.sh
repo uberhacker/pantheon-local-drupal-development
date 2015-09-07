88189796dec6f3687319dfcd7e41b9777e3a6fe3
uberhacker <edreel@gmail.com>
2015-09-07 00:06:00 -0500

    Added logic to detect if XHProf is already installed in xhprof-install.sh

--- xhprof-install.sh
+++ xhprof-install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+XHPROF=$(dpkg -l | grep php5-xhprof)
+if [ ! -z "$XHPROF" ]; then
+  echo "XHProf is already installed."
+  exit
+fi
 sudo apt-get install php5-xhprof
 sudo sed -i 's,/var/run/php5-fpm.sock,/var/run/php5-fpm.sock -idle-timeout 120,g' /etc/apache2/mods-available/fastcgi.conf
 /vagrant/restart-lamp.sh
