c4929cb1debf8af134b90bccb1883cfa106a189e
uberhacker <edreel@gmail.com>
2015-09-06 23:58:38 -0500

    Added logic to detect if codespell is already installed in codespell-install.sh

--- codespell-install.sh
+++ codespell-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+if [ -d "$HOME/.codespell" ]; then
+  echo "Codespell is already installed."
+  exit
+fi
 sudo apt-get install python3 -y
-git clone https://github.com/lucasdemarchi/codespell.git ~/.codespell
-cd ~/.codespell
+git clone https://github.com/lucasdemarchi/codespell.git $HOME/.codespell
+cd $HOME/.codespell
 sudo make install
