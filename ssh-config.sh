644e6aefc8714ae994c5725f953521c9cea2b85e
uberhacker <edreel@gmail.com>
2015-09-11 13:06:44 -0500

    Removed file check in ssh-config.sh

--- ssh-config.sh
+++ ssh-config.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-if [ ! -f $HOME/.ssh/rsa_id.pub ]; then
-  ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
-  echo ""
-  echo "To enable passwordless access, add the public ssh key below to your Pantheon account.  See https://pantheon.io/docs/articles/users/loading-ssh-keys/."
-  echo ""
-  cat $HOME/.ssh/id_rsa.pub
-  echo ""
-fi
+ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
+echo ""
+echo "To enable passwordless access, add the public ssh key below to your Pantheon account.  See https://pantheon.io/docs/articles/users/loading-ssh-keys/."
+echo ""
+cat $HOME/.ssh/id_rsa.pub
+echo ""
