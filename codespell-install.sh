433844ae591ff1d632106c1f52cfa71ea965c5ce
uberhacker <edreel@gmail.com>
2015-09-12 14:10:23 -0500

    Added required help2man package to codespell-install.sh

--- codespell-install.sh
+++ codespell-install.sh
@@ -3,7 +3,7 @@ if [ -d "$HOME/.codespell" ]; then
   echo "Codespell is already installed."
   exit
 fi
-sudo apt-get install python3 -y
+sudo apt-get install python3 help2man -y
 git clone https://github.com/lucasdemarchi/codespell.git $HOME/.codespell
 cd $HOME/.codespell
 sudo make install
