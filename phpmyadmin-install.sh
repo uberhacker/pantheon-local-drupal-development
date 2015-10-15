37334716d0358e5f71e4a9f2eb03adc3145bdd34
Ed Reel <ed.reel@drupalconnect.com>
2015-10-15 03:17:28 +0000

    Updated phpmyadmin.sh for Nginx.

--- phpmyadmin-install.sh
+++ phpmyadmin-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install debconf-utils -y
-echo phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2 | sudo debconf-set-selections
 sudo DEBIAN_FRONTEND=noninteractive apt-get install phpmyadmin -y
+sudo ln -s /usr/share/phpmyadmin/ /var/www/html/phpmyadmin
+/vagrant/restart-lamp.sh
 echo "Browse to http://192.168.33.10/phpmyadmin and login with Username: drupal and Password: drupal"
