12faeb1355e852bb6ec2860ca11af8b1c3dcf9f2
Ed Reel <ed.reel@drupalconnect.com>
2015-10-15 03:21:32 +0000

    Removed Apache config from xhprof-install.sh.

--- xhprof-install.sh
+++ xhprof-install.sh
@@ -4,6 +4,6 @@ if [ ! -z "$XHPROF" ]; then
   echo "XHProf is already installed."
   exit
 fi
-sudo apt-get install php5-xhprof
-sudo sed -i 's,/var/run/php5-fpm.sock,/var/run/php5-fpm.sock -idle-timeout 120,g' /etc/apache2/mods-available/fastcgi.conf
+echo "Installing XHProf..."
+sudo apt-get install php5-xhprof -y
 /vagrant/restart-lamp.sh
