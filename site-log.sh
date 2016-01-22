6e4a559ef5affa151143d41f3e80bb5a9d7a716f
Ed Reel <ed.reel@drupalconnect.com>
2016-01-22 17:56:25 +0000

    Updated Pantheon site name detection logic in site-log.sh

--- site-log.sh
+++ site-log.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
+
+# Get the Pantheon site name
+SITE=""
+if test $1; then
+  SITE=$1
+fi
+
 # Get the environment
 ENV=dev
 if test $2; then
   ENV=$2
 fi
 
-# Get the Pantheon site name
-if test $1; then
-  SITE=$1
-  DIR=$SITE-$ENV
-  if [ ! -d /var/www/$DIR ]; then
-    echo "$DIR is not a valid site directory."
-    exit
-  fi
-else
-  ROOT=$(drush status root --format=list)
-  if [ ! -z $ROOT ]; then
-    BASE=${ROOT:0:8}
-    if [ $BASE == "/var/www" ]; then
-      DIR=${ROOT:9}
-      END="-$ENV"
-      LEN=${#END}
-      SITE=${DIR:0:(-$LEN)}
-    fi
+# Set the Drupal root directory
+ROOT=$($DRUSH status root --format=list)
+if [ -z $ROOT ]; then
+  ROOT=/var/www/$SITE-$ENV
+fi
+
+# Validate the Drupal root directory
+if [ ! -d $ROOT ]; then
+  echo "The Pantheon site cannot be located."
+  exit
+fi
+
+# Get the Pantheon site name from Drupal root
+if [ -z $SITE ]; then
+  BASE=${ROOT:0:8}
+  if [ $BASE == "/var/www" ]; then
+    DIR=${ROOT:9}
+    END="-$ENV"
+    LEN=${#END}
+    SITE=${DIR:0:(-$LEN)}
   fi
 fi
 
-if [[ ! -z "$SITE" && ! -z "$DIR" ]]; then
+if [ ! -z $SITE ]; then
   TYPE=error
   if test $3; then
     TYPE=$3
@@ -43,7 +52,7 @@ if [[ ! -z "$SITE" && ! -z "$DIR" ]]; then
     echo "Valid options are less or tail.  The default value is tail."
     exit
   fi
-  sudo $CMD /var/log/nginx/$DIR-$TYPE.log
+  sudo $CMD /var/log/nginx/$SITE-$ENV-$TYPE.log
 else
   echo ""
   echo "Purpose: Display the web server logs for a site"
