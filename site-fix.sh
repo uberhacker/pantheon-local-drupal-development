9d3e4492519d113ba2aafeb83a93c2e784410648
Ed Reel <ed.reel@drupalconnect.com>
2016-01-22 17:55:57 +0000

    Updated Pantheon site name detection logic in site-fix.sh

--- site-fix.sh
+++ site-fix.sh
@@ -1,43 +1,60 @@
 #!/bin/bash
+
+# Check for prerequisite
+DRUSH=$(which drush)
+if [ $? == 1 ]; then
+  echo "Drush is not installed.  See http://www.drush.org/en/master/install."
+  exit
+fi
+
+# Get the Pantheon site name
+SITE=""
+if test $1; then
+  SITE=$1
+fi
+
 # Get the environment
 ENV=dev
 if test $2; then
   ENV=$2
 fi
 
-# Get the Pantheon Site Name
-if test $1; then
-  SITE=$1
-  DIR=$SITE-$ENV
-  if [ ! -d /var/www/$DIR ]; then
-    echo "$DIR is not a valid site directory."
-    exit
-  fi
-else
-  ROOT=$(drush status root --format=list)
-  if [ ! -z $ROOT ]; then
-    BASE=${ROOT:0:8}
-    if [ $BASE == "/var/www" ]; then
-      DIR=${ROOT:9}
-      END="-$ENV"
-      LEN=${#END}
-      SITE=${DIR:0:(-$LEN)}
-    fi
+# Set the Drupal root directory
+ROOT=$($DRUSH status root --format=list)
+if [ -z $ROOT ]; then
+  ROOT=/var/www/$SITE-$ENV
+fi
+
+# Validate the Drupal root directory
+if [ ! -d $ROOT ]; then
+  echo "The Pantheon site cannot be located."
+  exit
+fi
+
+# Get the Pantheon site name from Drupal root
+if [ -z $SITE ]; then
+  BASE=${ROOT:0:8}
+  if [ $BASE == "/var/www" ]; then
+    DIR=${ROOT:9}
+    END="-$ENV"
+    LEN=${#END}
+    SITE=${DIR:0:(-$LEN)}
   fi
 fi
-if [[ ! -z "$SITE" && ! -z "$DIR" ]]; then
+
+if [ ! -z $SITE ]; then
   if [ ! -f $HOME/.drush/registry_rebuild/registry_rebuild.php ]; then
-    drush dl registry_rebuild -y
-    drush cc drush
+    $DRUSH dl registry_rebuild -y
+    $DRUSH cc drush
   fi
 
   # Set multisite
   MULTISITES=""
   DEFAULTSITE="default"
-  cd /var/www/$DIR/sites
+  cd $ROOT/sites
   SITES=$(echo $(ls -d */) | sed 's,/,,g')
   for S in $SITES; do
-    if [[ "$S" != "all" && -f "/var/www/$DIR/sites/$S/settings.php" ]]; then
+    if [[ "$S" != "all" && -f "$ROOT/sites/$S/settings.php" ]]; then
       if [ -z "$MULTISITES" ]; then
         MULTISITES="$S"
       else
@@ -71,20 +88,20 @@ if [[ ! -z "$SITE" && ! -z "$DIR" ]]; then
       exit
     fi
   fi
-  cd /var/www/$DIR
+  cd $ROOT
   # Make sure the directory is writable by Nginx so files can be saved.
   sudo chown -R vagrant:www-data sites/$MULTISITE/files
   sudo chmod -R g+w sites/$MULTISITE/files
   # Make sure the directory is writable by Nginx so features can be exported.
-  if [ -d "/var/www/$DIR/sites/$MULTISITE/features" ]; then
+  if [ -d "$ROOT/sites/$MULTISITE/features" ]; then
     sudo chown -R vagrant:www-data sites/$MULTISITE/features
     sudo chmod -R g+w sites/$MULTISITE/features
   fi
-  if [ -d "/var/www/$DIR/sites/$MULTISITE/modules/features" ]; then
+  if [ -d "$ROOT/sites/$MULTISITE/modules/features" ]; then
     sudo chown -R vagrant:www-data sites/$MULTISITE/modules/features
     sudo chmod -R g+w sites/$MULTISITE/modules/features
   fi
-  if [ -d "/var/www/$DIR/sites/$MULTISITE/modules/custom/features" ]; then
+  if [ -d "$ROOT/sites/$MULTISITE/modules/custom/features" ]; then
     sudo chown -R vagrant:www-data sites/$MULTISITE/modules/custom/features
     sudo chmod -R g+w sites/$MULTISITE/modules/custom/features
   fi
